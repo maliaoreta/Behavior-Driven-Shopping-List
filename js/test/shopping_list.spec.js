da3ce9d27e591df8b7a7e89c9466c215bb185680
maliaoreta <maliaoreta@gmail.com>
2016-03-24 16:08:58 -1000

    adds test for description

--- js/test/shopping_list.spec.js
+++ js/test/shopping_list.spec.js
@@ -5,12 +5,23 @@ describe('ShoppingListItem', function () {
     expect(ShoppingListItem).to.be.a('function');
   });
 
+  //Checks if name property exists
   describe('name', function () {
 
-    it('should be a property in Shopping', function () {
+    it('should be a property in ShoppingListItem', function () {
 
       var nameCheck = new ShoppingListItem();
       expect(nameCheck).to.have.property('name');
+    })
+  })
+
+  // Checks if description property exists
+  describe('description', function () {
+
+    it('should be a property in ShoppingListItem', function () {
+
+      var describeCheck = new ShoppingListItem();
+      expect(describeCheck).to.have.property('description');
     })
   })
 });
