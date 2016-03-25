7370175a1bfe6bba741beb45734b58e22fe61652
maliaoreta <maliaoreta@gmail.com>
2016-03-24 18:09:11 -1000

    tests that this.name is set with the passed in argument

--- js/test/shopping_list.spec.js
+++ js/test/shopping_list.spec.js
@@ -1,37 +1,44 @@
 describe('ShoppingListItem', function () {
 
+  var SLI = null;
+
+  beforeEach(function () {
+
+    SLI = new ShoppingListItem();
+  })
+
   it('should be a class', function () {
 
     expect(ShoppingListItem).to.be.a('function');
   });
 
-  //Checks if name property exists
-  describe('name', function () {
+  // Checks if name is a property
+  it('should be a property in ShoppingListItem', function () {
 
-    it('should be a property in ShoppingListItem', function () {
-
-      var nameCheck = new ShoppingListItem();
-      expect(nameCheck).to.have.property('name');
-    });
+    expect(SLI).to.have.property('name');
   });
 
-  // Checks if description property exists
-  describe('description', function () {
+  // Checks if description is a property  
+  it('should be a property in ShoppingListItem', function () {
 
-    it('should be a property in ShoppingListItem', function () {
-
-      var describeCheck = new ShoppingListItem();
-      expect(describeCheck).to.have.property('description');
+      expect(SLI).to.have.property('description');
     });
-  });
 
   //Checks if is_done is a property
-  describe('is_done', function () {
+  it('should be a property in ShoppingListItem', function () {
 
-    it('should be a property in ShoppingListItem', function () {
+    expect(SLI).to.have.property('is_done');
+   });
 
-      var isDone = new ShoppingListItem();
-      expect(isDone).to.have.property('is_done');
+  // Check constructor method
+  describe('constructor method', function () {
+
+    var constructorTest = new ShoppingListItem('pretzel', 'snack item');
+
+    it('should accept name as an argument and set it using the passed in argument', function () {
+
+      expect(constructorTest.name).to.equal('pretzel');
     })
   })
-});
+
+;});
