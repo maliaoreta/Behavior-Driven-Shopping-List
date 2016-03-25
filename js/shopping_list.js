16866d77248e65a4c0e33099a8d7760e273b5724
maliaoreta <maliaoreta@gmail.com>
2016-03-25 11:29:20 -1000

    addItem method adds shoppingListItem argument to this.items array

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -3,6 +3,7 @@ var ShoppingList = function () {
   this.items = [];
 }
 
-ShoppingList.prototype.addItem = function () {
-  
+ShoppingList.prototype.addItem = function (shoppingListItem) {
+ 
+  return this.items.push(shoppingListItem);
 }
