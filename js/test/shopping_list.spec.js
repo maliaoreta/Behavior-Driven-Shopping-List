118e4a6f3fad167fe99dee5f1f2c9f615b23a7c2
maliaoreta <maliaoreta@gmail.com>
2016-03-24 18:27:58 -1000

    tests if uncheck method sets is_done to false

--- js/test/shopping_list.spec.js
+++ js/test/shopping_list.spec.js
@@ -68,8 +68,13 @@ describe('ShoppingListItem', function () {
     it('should be a method on ShoppingListItem', function () {
 
       expect(SLI.uncheck).to.be.a('function');
+    });
+
+    it('should set is_done to false', function () {
+
+      expect(SLI.uncheck()).to.be.false;
     })
-  })
+  });
 
 
 ;});
