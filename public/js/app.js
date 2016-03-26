21c4ebca59aeff1c91144e52abd3ad32c5123445
maliaoreta <maliaoreta@gmail.com>
2016-03-26 11:39:02 -1000

    adds preventDefault to form so that the rendered shopping list items stay on page

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,11 @@ function add_to_shopping_list () {
   var title = document.getElementById('title').value;
   var description = document.getElementById('description').value;
   var new_shopping_list_item = new ShoppingListItem(title, description);
-
   shopping_list.addItem(new_shopping_list_item);
   renderedList = shopping_list.render();
+  document.getElementById('content').innerHTML = renderedList;
 }
+  document.getElementById('form').addEventListener('click', function (event) {
+
+    event.preventDefault();
+  });
