5526582eb040656607c897c243db775f88180b75
maliaoreta <maliaoreta@gmail.com>
2016-03-25 19:20:33 -1000

    fixes formatting of html output

--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -17,12 +17,7 @@ ShoppingListItem.prototype.uncheck = function () {
 
 ShoppingListItem.prototype.render = function () {
 
-  var htmlOutput = "<ul> \
-        <li class=\"completed_" + this.is_done + "\"> \
-        <span>" + this.name + "</span> \
-        <span>" + this.description + "</span> \
-        </li> \
-        <ul>";
+  var htmlOutput = "<li class=\"completed_" + this.is_done + "\"> \n<span>" + this.name + "</span> \n<span>" + this.description + "</span> \n</li>";
 
   return htmlOutput;
 };
