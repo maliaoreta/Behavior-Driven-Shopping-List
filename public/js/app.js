2ff7cff04b5ab5de86c71ee7f76023b90d59982f
maliaoreta <maliaoreta@gmail.com>
2016-03-28 12:33:29 -1000

    adds removeItemButtonClicked function and rerendering in changeCheckedStatus function

--- public/js/app.js
+++ public/js/app.js
@@ -13,6 +13,7 @@ function add_to_shopping_list () {
   var title = document.getElementById('title').value;
   var description = document.getElementById('description').value;
   var new_shopping_list_item = new ShoppingListItem(title, description);
+  
   shopping_list.addItem(new_shopping_list_item);
   renderedList = shopping_list.render();
   document.getElementById('content').innerHTML = renderedList;
@@ -22,9 +23,19 @@ function changeCheckedStatus (idx, checkbox) {
  
   if (checkbox.checked === true) {
 
-  shopping_list.items[idx].check();
+    shopping_list.items[idx].check();
+    renderedList = shopping_list.render();
+    checkbox.checked = true;
+    document.getElementById('content').innerHTML = renderedList;
   }
   else {
-  shopping_list.items[idx].uncheck();
+    shopping_list.items[idx].uncheck();
   }
 };
+
+function removeItemButtonClicked (idx) {
+
+  shopping_list.removeItem(shopping_list.items[idx]);
+  renderedList = shopping_list.render();
+  document.getElementById('content').innerHTML = renderedList;
+}
